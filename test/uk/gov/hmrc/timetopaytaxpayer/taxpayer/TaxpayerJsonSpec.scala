89368f28381137b85a99a0680199249ce1f850b4
Paweł Panasewicz <2970040+pawelpanasewicz@users.noreply.github.com>
2020-01-29 13:53:46 +0000

    [PP] moving functionality of filtering old returns from frontend to taxpayer-backend (#25)

--- test/uk/gov/hmrc/timetopaytaxpayer/taxpayer/TaxpayerJsonSpec.scala
+++ test/uk/gov/hmrc/timetopaytaxpayer/taxpayer/TaxpayerJsonSpec.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 HM Revenue & Customs
+ * Copyright 2020 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,7 +16,8 @@
 
 package uk.gov.hmrc.timetopaytaxpayer.taxpayer
 
-import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.time.{Clock, LocalDate, LocalDateTime, ZoneId}
 
 import play.api.libs.json._
 import org.scalatest.{Matchers, WordSpec}
@@ -139,6 +140,111 @@ class TaxpayerJsonSpec extends WordSpec with Matchers {
       }
     }
 
+  }
+
+  "fix returns" in {
+
+      implicit def stringToDate(s: String): LocalDate = LocalDate.parse(s)
+      implicit def stringToDateO(s: String): Option[LocalDate] = Some(LocalDate.parse(s))
+
+    val saBeforeFix = SelfAssessmentDetails(
+      utr                      = SaUtr("1234567890"),
+      communicationPreferences = CommunicationPreferences(welshLanguageIndicator = false, audioIndicator = false, largePrintIndicator = false, brailleIndicator = false),
+      debits                   = List(
+        Debit(
+          originCode = "POA2",
+          amount     = 250.52,
+          dueDate    = LocalDate.parse("2016-01-31"),
+          interest   = Some(Interest(Some(LocalDate.parse("2016-06-01")), 42.32)),
+          taxYearEnd = LocalDate.parse("2017-04-05")
+        )
+      ),
+      returns                  = List(
+        Return(
+          taxYearEnd   = "2000-04-05",
+          issuedDate   = "2001-01-23",
+          dueDate      = "2001-04-30",
+          receivedDate = "2001-04-10"
+        ),
+        Return(
+          taxYearEnd   = "2001-04-10",
+          issuedDate   = "2001-04-06",
+          dueDate      = "2002-01-31",
+          receivedDate = "2001-06-19"
+        ),
+        Return(
+          taxYearEnd   = "2002-04-05",
+          issuedDate   = "2002-04-06",
+          dueDate      = "2003-01-31",
+          receivedDate = "2002-05-08"
+        ),
+        Return(
+          taxYearEnd   = "2003-04-05",
+          issuedDate   = "2003-04-06",
+          dueDate      = "2004-01-31",
+          receivedDate = "2003-07-04"
+        ),
+        Return(
+          taxYearEnd   = "2004-04-05",
+          issuedDate   = "2004-04-06",
+          dueDate      = "2005-01-31",
+          receivedDate = "2004-08-23"
+        ),
+        Return(
+          taxYearEnd   = "2005-04-05",
+          issuedDate   = None,
+          dueDate      = None,
+          receivedDate = None
+        )
+      )
+    )
+    val saAfterFix = SelfAssessmentDetails(
+      utr                      = SaUtr("1234567890"),
+      communicationPreferences = CommunicationPreferences(welshLanguageIndicator = false, audioIndicator = false, largePrintIndicator = false, brailleIndicator = false),
+      debits                   = List(
+        Debit(
+          originCode = "POA2",
+          amount     = 250.52,
+          dueDate    = LocalDate.parse("2016-01-31"),
+          interest   = Some(Interest(Some(LocalDate.parse("2016-06-01")), 42.32)),
+          taxYearEnd = LocalDate.parse("2017-04-05")
+        )
+      ),
+      returns                  = List(
+        Return(
+          taxYearEnd   = "2001-04-10",
+          issuedDate   = "2001-04-06",
+          dueDate      = "2002-01-31",
+          receivedDate = "2001-06-19"
+        ),
+        Return(
+          taxYearEnd   = "2002-04-05",
+          issuedDate   = "2002-04-06",
+          dueDate      = "2003-01-31",
+          receivedDate = "2002-05-08"
+        ),
+        Return(
+          taxYearEnd   = "2003-04-05",
+          issuedDate   = "2003-04-06",
+          dueDate      = "2004-01-31",
+          receivedDate = "2003-07-04"
+        ),
+        Return(
+          taxYearEnd   = "2004-04-05",
+          issuedDate   = "2004-04-06",
+          dueDate      = "2005-01-31",
+          receivedDate = "2004-08-23"
+        ),
+        Return(
+          taxYearEnd   = "2005-04-05",
+          issuedDate   = None,
+          dueDate      = None,
+          receivedDate = None
+        )
+      )
+    )
+    implicit val clock: Clock = Clock.fixed(LocalDateTime.parse("2006-01-22T16:28:55.185", DateTimeFormatter.ISO_DATE_TIME).atZone(ZoneId.of("Europe/London")).toInstant, ZoneId.of("UTC"))
+    saBeforeFix.fixReturns shouldBe saAfterFix
   }
 
 }

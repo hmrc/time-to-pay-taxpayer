0f1e78b6cbecd5a898342571d6e18646ac334abd
Kostas Ntanos <kostas.ntanos@digital.hmrc.gov.uk>
2023-08-10 12:42:22 +0100

    [KN][OPS-9514] Change list of excludedfiles for scoverage

--- project/ScoverageSettings.scala
+++ project/ScoverageSettings.scala
@@ -5,20 +5,8 @@ import scoverage.ScoverageKeys
 object ScoverageSettings {
 
   def apply(): Seq[Def.Setting[? >: String & Double & Boolean]] = Seq( // Semicolon-separated list of regexes matching classes to exclude
-    ScoverageKeys.coverageExcludedPackages := """uk\.gov\.hmrc\.BuildInfo;.*\.Routes;.*\.RoutesPrefix;.*\.Reverse[^.]*;testonly""",
-    ScoverageKeys.coverageExcludedFiles := Seq(
-      "" +
-        "<empty>",
-      "Reverse.*",
-      ".*models.*",
-      ".*repositories.*",
-      ".*BuildInfo.*",
-      ".*javascript.*",
-      ".*Routes.*",
-      ".*GuiceInjector",
-      ".*DateTimeQueryStringBinder.*", // better covered via wiremock/E2E integration tests
-      ".*Test.*"
-    ).mkString(";"),
+    ScoverageKeys.coverageExcludedPackages := "<empty>;.*BuildInfo.*;Reverse.*;app.Routes.*;prod.*;testOnlyDoNotUseInProd.*;manualdihealth.*;forms.*;config.*;",
+    ScoverageKeys.coverageExcludedFiles := ".*microserviceGlobal.*;.*microserviceWiring.*;.*ApplicationLoader.*;.*ApplicationConfig.*;.*package.*;.*Routes.*;.*TestOnlyController.*;.*WebService.*",
     ScoverageKeys.coverageMinimumStmtTotal := 80, //should be a lot higher but we are where we are
     ScoverageKeys.coverageFailOnMinimum := false,
     ScoverageKeys.coverageHighlighting := true,

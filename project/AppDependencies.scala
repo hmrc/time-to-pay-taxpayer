a1d08e4da9ae2819c4582be19536e87321964962
Jack Gilman <56401347+DreamcastKnight@users.noreply.github.com>
2024-08-29 11:50:18 +0100

    [JG] OPS-12665 - dependency upgrades (#60)
    * [JG] OPS-12665 - dependency upgrades
    
    * [JG] OPS-12665 - update sbt plugin

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,7 +5,7 @@ import sbt.librarymanagement.Configurations.Provided
 
 object AppDependencies {
 
-  val bootstrapVersion = "9.0.0"
+  val bootstrapVersion = "9.4.0"
   val cryptoVersion = "7.6.0"
 
   val compile = Seq(
@@ -15,10 +15,10 @@ object AppDependencies {
 
   val test = Seq(
     "uk.gov.hmrc"            %% "bootstrap-test-play-30"  % bootstrapVersion,
-    "org.scalatest"          %% "scalatest"               % "3.2.18",
+    "org.scalatest"          %% "scalatest"               % "3.2.19",
     "org.scalatestplus.play" %% "scalatestplus-play"      % "7.0.1",
-    "org.wiremock"            % "wiremock-standalone"     % "3.6.0",
-    "org.mockito"             % "mockito-core"            % "5.12.0"
+    "org.wiremock"            % "wiremock-standalone"     % "3.9.1",
+    "org.mockito"             % "mockito-core"            % "5.13.0"
   ).map(_ % Test)
 
   val corDependencies = List(

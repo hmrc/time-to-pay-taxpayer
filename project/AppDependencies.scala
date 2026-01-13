4a5ab7515e394f5cce58e564e5ed4ba4658d2cb2
Adam Banner <120569454+adambanner1904@users.noreply.github.com>
2026-01-13 11:34:15 +0000

    [AB][OPS-14479] Updating dependencies (#67)
    * [AB][OPS-14479] Updating dependencies
    
    * Updating plug in dependency

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,7 +5,7 @@ import sbt.librarymanagement.Configurations.Provided
 
 object AppDependencies {
 
-  val bootstrapVersion = "10.1.0"
+  val bootstrapVersion = "10.5.0"
   val cryptoVersion = "7.6.0"
 
   val compile = Seq(
@@ -17,8 +17,8 @@ object AppDependencies {
     "uk.gov.hmrc"            %% "bootstrap-test-play-30"  % bootstrapVersion,
     "org.scalatest"          %% "scalatest"               % "3.2.19",
     "org.scalatestplus.play" %% "scalatestplus-play"      % "7.0.2",
-    "org.wiremock"            % "wiremock-standalone"     % "3.13.1",
-    "org.mockito"             % "mockito-core"            % "5.19.0"
+    "org.wiremock"            % "wiremock-standalone"     % "3.13.2",
+    "org.mockito"             % "mockito-core"            % "5.21.0"
   ).map(_ % Test)
 
   val corDependencies = List(
